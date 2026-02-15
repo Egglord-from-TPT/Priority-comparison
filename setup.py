b4049609731b912ea50eac423ccf1ef36d002d02
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-02-15 17:53:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="variables_plus",
-    version="2.2.1",
+    version="2.2.2",
     packages=find_packages(),
     description="A simple library to create, clear, and delete global variables",
     author="",
