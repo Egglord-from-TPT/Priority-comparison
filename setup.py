832d25de3363f5ffd7bf99989d50cb0d412516a5
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-02-25 17:56:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prioritycomparison",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(),
     description="A simple Python library that lets you check comparisons in order of priority (importance).",
     author="",
